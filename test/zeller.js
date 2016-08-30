9efd07aa6f7682a7bf4b810924568762fa1b629a
Matt Latham <latham75@gmail.com>
2016-08-30 14:10:27 -0500

    zeller testing

--- test/zeller.js
+++ test/zeller.js
@@ -1,2 +1,16 @@
 "use strict";
 
+const {exec} = require('child_process');
+const {assert: {isFunction, strictEqual}} = require('chai');
+const {getDayOfWeek} = require('../lib/zeller')
+
+describe ('zeller', () => {
+	it('should be a function', () => {
+		isFunction(getDayOfWeek)
+	})
+	it('should return day number', () => {
+		const input = 30;
+		const expected = new Date().getDay()+1
+		strictEqual(getDayOfWeek(input), expected)
+		})
+	})
