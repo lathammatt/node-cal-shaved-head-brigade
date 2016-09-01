3c5adfa642fbf7f024079764be5f9f49f8bc5383
Matt Latham <latham75@gmail.com>
2016-09-01 14:21:09 -0500

    array of days correct for months

--- test/zeller.js
+++ test/zeller.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const {exec} = require('child_process');
-const {assert: {isFunction, strictEqual, isObject}} = require('chai');
-const {getDayOfWeek, getMonth, getYear, monthName, weekStart, monthCalc, yearCalc} = require('../lib/zeller')
+const {assert: {isFunction, strictEqual, isArray}} = require('chai');
+const {getDayOfWeek, getMonth, getYear, monthName, weekStart, monthCalc, yearCalc, monthBuild, monthDays} = require('../lib/zeller')
 
 // describe ('zeller', () => {
 // 	it('should be a function', () => {
@@ -89,12 +89,41 @@ describe('zeller', () => {
 		})
 	})
 
-	describe('week start', () => {
+	describe('days in month', () => {
+		it('should be a function', () => {
+			isFunction(monthDays)
+		})
+		it('should return a number', () => {
+			strictEqual(monthDays(1), 31)
+			strictEqual(monthDays(2), 28)
+			strictEqual(monthDays(9), 30)
+		})
+	})
+
+	describe('start of week', () => {
 		it('should be a function', () => {
 			isFunction(weekStart)
 		})
-		it.skip('should')
+		it('should return an array', () => {
+			let expected = ["   ", "   ", "   "]
+			isArray(weekStart(3), expected)
+		})
 	})
+
+	describe('building the month', () => {
+		it('should be a function', () => {
+			isFunction(monthBuild)
+		})
+		it('should return an array', () => {
+			let expected = ["   ", "   ", "   ", "   ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", "10 ", "11 ", "12 ", "13 ", "14 ", "15 ", "16 ", "17 ", "18 ", "19 ", "20 ", "21 ", "22 ", "23 ", "24 ", "25 ", "26 ", "27 ", "28 ", "29 ", "30 "]
+			let expected2 = ["   ", "   ", "   ", "   ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", "10 ", "11 ", "12 ", "13 ", "14 ", "15 ", "16 ", "17 ", "18 ", "19 ", "20 ", "21 ", "22 ", "23 ", "24 ", "25 ", "26 ", "27 ", "28 ", "29 ", "30 ", "31 "]
+			let expected3 = ["   ", "   ", "   ", "   ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", "10 ", "11 ", "12 ", "13 ", "14 ", "15 ", "16 ", "17 ", "18 ", "19 ", "20 ", "21 ", "22 ", "23 ", "24 ", "25 ", "26 ", "27 ", "28 "]
+			isArray(monthBuild(2016, 9, 1), expected)
+			isArray(monthBuild(2016, 12, 1), expected2)
+			isArray(monthBuild(2015, 2), expected3)
+		})
+	})
+
 })
 
 
